5b4b3e7645b4e8dcf57e123f64a16d82279d33c1
huanyu.yang <mfuture@qq.com>
2023-02-14 14:13:20 +0800

    Update filter build script and input file

--- server/clan/filter/build.sh
+++ server/clan/filter/build.sh
@@ -1,6 +1,7 @@
-rm -rf filtering_video.o transcoding.o
+rm -rf /opt/application/tx-rtcStream/server/clan/filter/filtering_video.o /opt/application/tx-rtcStream/server/clan/filter/transcoding.o
+cp /opt/application/tx-rtcStream/server/clan/filter/input_bak.txt /opt/application/tx-rtcStream/server/clan/filter/input.txt
 
 make
-gcc -fPIC -shared -rdynamic  filtering_video.o -L/opt/program/ffmpeg/lib -lavcodec -lavformat -lavfilter -lavutil -lavdevice -o filtering_video.so
+gcc -fPIC -shared -rdynamic  /opt/application/tx-rtcStream/server/clan/filter/filtering_video.o -L/opt/program/ffmpeg/lib -lavcodec -lavformat -lavfilter -lavutil -lavdevice -o /opt/application/tx-rtcStream/server/clan/filter/filtering_video.so
 
-gcc -fPIC -shared -rdynamic  transcoding.o -L/opt/program/ffmpeg/lib -lavcodec -lavformat -lavfilter -lavutil -lavdevice -o transcoding.so
+gcc -fPIC -shared -rdynamic  /opt/application/tx-rtcStream/server/clan/filter/transcoding.o -L/opt/program/ffmpeg/lib -lavcodec -lavformat -lavfilter -lavutil -lavdevice -o /opt/application/tx-rtcStream/server/clan/filter/transcoding.so
