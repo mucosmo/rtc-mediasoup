54072d45002db60fbca02f5c2d6d683e65af7d66
huanyu.yang <mfuture@qq.com>
2023-02-15 13:12:38 +0800

    update ffmeg C program test file

--- server/clan/filter/test.js
+++ server/clan/filter/test.js
@@ -1,7 +1,7 @@
 var ffi = require('ffi-napi');
 
 const fs = require('fs');
-var filter = ffi.Library('./filter', {
+var filter = ffi.Library('./filtering_video', {
     'avfilter': ['int', ['string', 'string', 'string']],
     "pushStream": ['int', ['string', 'string']],
 
@@ -11,11 +11,12 @@ const dhVideo = '/opt/application/tx-rtcStream/files/resources/dh.mp4'
 const offiveVideo = '/opt/application/tx-rtcStream/files/resources/filevideo.mp4';
 const input = '/opt/application/tx-rtcStream/server/clan/input.txt';
 const output = '/opt/application/tx-rtcStream/server/clan/output.txt';
+const me_20s = '/opt/application/tx-rtcStream/files/resources/me_voice_20s.mp4';
 
-const rtmp = 'rtmp://121.5.133.154:1935/myapp/12345';
+const rtmp = 'rtmp://121.5.133.154:1935/myapp/1234';
 
 // filter.avfilter(offiveVideo, input, rtmp);
-filter.pushStream(offiveVideo, rtmp);
+filter.pushStream(me_20s, rtmp);
 
 
 
