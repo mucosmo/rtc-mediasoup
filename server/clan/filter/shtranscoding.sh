012c365e922d927df5e7610f597312218db71d44
huanyu.yang <mfuture@qq.com>
2023-02-17 14:01:09 +0800

    update laucnh and shtranscoding.sh

--- server/clan/filter/shtranscoding.sh
+++ server/clan/filter/shtranscoding.sh
@@ -7,7 +7,7 @@ outputvideo=/opt/application/tx-rtcStream/files/composites/c-${timestamp}.mp4
 
 
 # RTMP 生成文件
-./transcoding /opt/application/tx-rtcStream/files/resources/screen5s.mp4 ${outputvideo}
+./transcoding /opt/application/tx-rtcStream/files/resources/office10s.mp4 ${outputvideo}
 
 end_time=$(date +%s)
 
