b961a0e61f975d0e06f6011923031bd6ec064c1a
huanyu.yang <mfuture@qq.com>
2023-02-20 07:50:00 +0800

    add  remove condition

--- server/clan/filter/rtp_build.sh
+++ server/clan/filter/rtp_build.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/bash
-rm -rf /opt/application/tx-rtcStream/server/clan/filter/send_by_rtp.o
+video=/opt/application/tx-rtcStream/server/clan/filter/send_by_rtp.o
+if [ -f $video ]; then
+  rm -rf $video
+fi
 
 make
 gcc -fPIC -shared -rdynamic  /opt/application/tx-rtcStream/server/clan/filter/send_by_rtp.o -L/opt/program/ffmpeg/lib -lavcodec -lavformat -lavfilter -lavutil -lavdevice -o /opt/application/tx-rtcStream/server/clan/filter/send_by_rtp.so
