79cb5a1722df2772d65146b1210bc1a726758e82
huanyu.yang <mfuture@qq.com>
2023-02-22 17:04:32 +0800

    change file path

--- lab/clan/filter/shtranscoding.sh
+++ lab/clan/filter/shtranscoding.sh
@@ -1,6 +1,6 @@
 
 timestamp=$(date +%s)
-outputvideo=/opt/application/tx-rtcStream/files/composites/c-${timestamp}.mp4
+outputvideo=/opt/application/tx-rtcStream/files/synthesis/c-${timestamp}.mp4
 
 # # RTMP 推流
 # ./transcoding /opt/application/tx-rtcStream/files/resources/screen5s.mp4 rtmp://121.5.133.154:1935/myapp/12345
