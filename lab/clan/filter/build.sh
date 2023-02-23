66f7fb798997c423a6db311577de2b6fe02ef7c6
huanyu.yang <mfuture@qq.com>
2023-02-23 19:48:24 +0800

    ok to push ts file to rtp

--- lab/clan/filter/build.sh
+++ lab/clan/filter/build.sh
@@ -1,7 +1,31 @@
-rm -rf /opt/application/tx-rtcStream/server/clan/filter/filtering_video.o /opt/application/tx-rtcStream/server/clan/filter/transcoding.o
-cp /opt/application/tx-rtcStream/server/clan/filter/input_bak.txt /opt/application/tx-rtcStream/server/clan/filter/input.txt
+echo "start to make..."
+
+video1=/opt/application/tx-rtcStream/lab/clan/filter/filtering_video.o
+video2=/opt/application/tx-rtcStream/lab/clan/filter/transcoding.o
+if [ -f $video1 ]; then
+  rm -rf $video1
+fi
+
+if [ -f $video2 ]; then
+  rm -rf $video2
+fi
+
+cp /opt/application/tx-rtcStream/lab/clan/filter/input_bak.txt /opt/application/tx-rtcStream/lab/clan/filter/input.txt
 
 make
-gcc -fPIC -shared -rdynamic  /opt/application/tx-rtcStream/server/clan/filter/filtering_video.o -L/opt/program/ffmpeg/lib -lavcodec -lavformat -lavfilter -lavutil -lavdevice -o /opt/application/tx-rtcStream/server/clan/filter/filtering_video.so
 
-gcc -fPIC -shared -rdynamic  /opt/application/tx-rtcStream/server/clan/filter/transcoding.o -L/opt/program/ffmpeg/lib -lavcodec -lavformat -lavfilter -lavutil -lavdevice -o /opt/application/tx-rtcStream/server/clan/filter/transcoding.so
+gcc -fPIC -shared -rdynamic  /opt/application/tx-rtcStream/lab/clan/filter/filtering_video.o -L/opt/program/ffmpeg/lib -lavcodec -lavformat -lavfilter -lavutil -lavdevice -o /opt/application/tx-rtcStream/lab/clan/filter/filtering_video.so
+gcc -fPIC -shared -rdynamic  /opt/application/tx-rtcStream/lab/clan/filter/transcoding.o -L/opt/program/ffmpeg/lib -lavcodec -lavformat -lavfilter -lavutil -lavdevice -o /opt/application/tx-rtcStream/lab/clan/filter/transcoding.so
+
+
+echo "transcoding..."
+timestamp=$(date +%s)
+outputvideo=/opt/application/tx-rtcStream/files/synthesis/output.ts
+
+# RTMP 生成文件
+./transcoding rtmp://liveplay.ivh.qq.com/live/m1015658666000385 ${outputvideo}
+
+end_time=$(date +%s)
+
+echo c-execution time was `expr $end_time - $timestamp` s
+
--- lab/clan/filter/rtp_build.sh
+++ lab/clan/filter/rtp_build.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/bash
-video=/opt/application/tx-rtcStream/server/clan/filter/send_by_rtp.o
+video=/opt/application/tx-rtcStream/lab/clan/filter/send_by_rtp.o
 if [ -f $video ]; then
   rm -rf $video
 fi
 
 make
-gcc -fPIC -shared -rdynamic  /opt/application/tx-rtcStream/server/clan/filter/send_by_rtp.o -L/opt/program/ffmpeg/lib -lavcodec -lavformat -lavfilter -lavutil -lavdevice -o /opt/application/tx-rtcStream/server/clan/filter/send_by_rtp.so
+gcc -fPIC -shared -rdynamic  /opt/application/tx-rtcStream/lab/clan/filter/send_by_rtp.o -L/opt/program/ffmpeg/lib -lavcodec -lavformat -lavfilter -lavutil -lavdevice -o /opt/application/tx-rtcStream/lab/clan/filter/send_by_rtp.so
 
