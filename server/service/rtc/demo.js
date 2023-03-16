5650c4edd268da53309bb76c21524601b7f85f36
huanyu.yang <mfuture@qq.com>
2023-03-16 13:18:13 +0800

    update demo

--- server/service/rtc/demo.js
+++ server/service/rtc/demo.js
@@ -2,22 +2,6 @@ const { RtcSDK } = require('./rtcSDK');
 
 const rtc = new RtcSDK({});
 
-
-let AsrSDK = null;
-import('/opt/dev/rtcSdk/server/lib/AsrSdk/AsrUtil.js').then(async mod => {
-    AsrSDK = mod.default;
-})
-
-module.exports.open = async (param) => {
-    asrUtil = new AsrSDK(param);
-    await asrUtil.open("TX_5G_ASR_TEST_");
-    global.asrUtil = asrUtil;
-
-}
-
-asrUtil = new AsrSDK(param);
-await asrUtil.open("TX_5G_ASR_TEST_");
-
 rtc.socketConnect().then(async () => {
 
     const ret = await rtc.roomStats();
