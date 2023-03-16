3837b4e4e52f2a0952eb1e6f40b8eb3ed0daee8f
huanyu.yang <mfuture@qq.com>
2023-03-16 13:04:39 +0800

    update demo

--- server/service/rtc/demo.js
+++ server/service/rtc/demo.js
@@ -2,6 +2,22 @@ const { RtcSDK } = require('./rtcSDK');
 
 const rtc = new RtcSDK({});
 
+
+let AsrSDK = null;
+import('/opt/dev/rtcSdk/server/lib/AsrSdk/AsrUtil.js').then(async mod => {
+    AsrSDK = mod.default;
+})
+
+module.exports.open = async (param) => {
+    asrUtil = new AsrSDK(param);
+    await asrUtil.open("TX_5G_ASR_TEST_");
+    global.asrUtil = asrUtil;
+
+}
+
+asrUtil = new AsrSDK(param);
+await asrUtil.open("TX_5G_ASR_TEST_");
+
 rtc.socketConnect().then(async () => {
 
     const ret = await rtc.roomStats();
@@ -10,6 +26,7 @@ rtc.socketConnect().then(async () => {
     rtc.pullAudio('1', ret[0]['members'][0]);
 
     rtc.on('newAudioData', (data) => {
+
         console.log(data)
     });
 });
