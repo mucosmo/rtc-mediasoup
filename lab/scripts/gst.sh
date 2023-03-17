64c3184440b661f6128c4f1e25383e730dacd523
huanyu.yang <mfuture@qq.com>
2023-03-17 13:50:00 +0800

    relocate gst snippet

---
+++ lab/scripts/gst.sh
@@ -0,0 +1,7 @@
+ 
+# 5 秒长的视频
+gst-launch-1.0 videotestsrc num-buffers=150 ! video/x-raw,width=1280,height=720,framerate=30/1 ! videoconvert ! x264enc ! mp4mux ! filesink location=output.mp4
+
+# pattern
+gst-launch-1.0 videotestsrc pattern=21 num-buffers=150 ! video/x-raw,width=1280,height=720,framerate=30/1 ! videoconvert ! x264enc ! mp4mux ! filesink location=output.mp4
+
--- scripts/gst.sh
+++
@@ -1,3 +0,0 @@
- 
-# 5 秒长的视频
-gst-launch-1.0 videotestsrc num-buffers=150 ! video/x-raw,width=1280,height=720,framerate=30/1 ! videoconvert ! x264enc ! mp4mux ! filesink location=output.mp4
