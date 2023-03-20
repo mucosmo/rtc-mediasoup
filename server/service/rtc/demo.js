13093e666cee63ac26c1bf022972d6139576c6d9
huanyu.yang <mfuture@qq.com>
2023-03-20 17:02:15 +0800

    update demo

--- server/service/rtc/demo.js
+++ server/service/rtc/demo.js
@@ -7,23 +7,21 @@ const fs = require('fs');
 rtc.socketConnect().then(async () => {
 
 
-    // rtc.pullAudio('1', ret[0]['members'][0]);
+    // 拉音频流
+    const ret = await rtc.roomStats();
+    rtc.pullAudio('1', ret[0]['members'][0]);
+    rtc.on('newAudioData', (data) => {
+        console.log(data)
+    });
 
-    // rtc.on('newAudioData', (data) => {
 
-    //     console.log(data)
-    // });
-
-    // const audio = fs.readFileSync('/opt/dev/rtcSdk/server/service/rtc/tts.text', 'utf-8');
-    // rtc.pushAuido(rtc.roomId, rtc.peerId, audio);
-
-    await rtc.createRoom({roomId: '21', userId: '4'});
-    await rtc.joinRoom();
-    rtc.pushStream('/opt/dev/rtcSdk/files/resources/40_input.mp4');
-
-    setTimeout(() => {
-        rtc.leaveRoom();
-    }, 15000);
+    // // 推视频流
+    // await rtc.createRoom({roomId: '21', userId: '4'});
+    // await rtc.joinRoom();
+    // rtc.pushStream('/opt/dev/rtcSdk/files/resources/40_input.mp4');
+    // setTimeout(() => {
+    //     rtc.leaveRoom();
+    // }, 15000);
 
 });
 
