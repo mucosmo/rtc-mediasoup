7842719121a100ff2db52eb97d41d5426d9290e4
huanyu.yang <mfuture@qq.com>
2023-04-11 13:44:07 +0800

    update rtcSDK demo

--- server/service/rtc/demo.js
+++ server/service/rtc/demo.js
@@ -2,27 +2,16 @@ const { RtcSDK } = require('./rtcSDK');
 
 const rtc = new RtcSDK({});
 
-const fs = require('fs');
-
-
-// // // 拉取音频 asr
+// // 拉取音频 asr
 // rtc.socketConnect().then(async () => {
 //     // const ret = await rtc.roomStats();
-//     let roomId = 'mixer';
-//     let peerId = 'node_ub4madtm53r';
+//     let roomId = '1';
+//     let peerId = 'browser_000119154nzzjd3bs3h';
 
-
-//     // 拉取音频
 //     rtc.pullAudio(roomId, peerId);
-//     rtc.on('message', (data) => {
+//     rtc.on('newAudioData', (data) => {
 //         console.log(data)
 //     });
-
-//     // // 正在讲话的用户
-//     // rtc.activeSpeaker();
-//     // rtc.on('message', (data) => {
-//     //     console.log(JSON.parse(data.toString()))
-//     // });
 
 //     // setTimeout(() => {
 //     //     rtc.socketClose();
@@ -30,26 +19,49 @@ const fs = require('fs');
 
 // });
 
+// // 正在讲话的用户
+// rtc.socketConnect().then(async () => {
+//     rtc.activeSpeaker();
+//     rtc.on('activeSpeaker', (data) => {
+//         console.log(data)
+//     });
+// });
+
+// // 房间动态
+// rtc.socketConnect().then(async () => {
+//     rtc.roomStatus();
+//     rtc.on('roomStatus', (data) => {
+//         console.log(data)
+//     });
+// });
+
 // // 推送音视频
-setTimeout(async () => {
-    // 创建房间
-    await rtc.joinRoom({ roomId: 'mixer', language: 'zh', target: { language: 'zh', role: 'student' } });
+// setTimeout(async () => {
+//     // 创建房间
+//     await rtc.joinRoom({
+//         roomId: '1',
+//         target: {
+//             lanListen: ['zh'],
+//             role: 'student'
+//         }
+//     });
 
-    // // 推送音视频
-    // await rtc.pushStream({ video: '/opt/dev/rtcSdk/files/resources/filevideo.mp4', audio: '/opt/dev/rtcSdk/files/resources/彩虹.mp3' });
-    // await rtc.pushStream({ video: '/opt/dev/rtcSdk/files/resources/filevideo.mp4' });
-    await rtc.pushStream({ audio: '/opt/dev/rtcSdk/files/resources/彩虹.mp3' });
+//     // // 推送音视频
+//     // await rtc.pushStream({ video: '/opt/dev/rtcSdk/files/resources/filevideo.mp4', audio: '/opt/dev/rtcSdk/files/resources/彩虹.mp3' });
+//     // await rtc.pushStream({ video: '/opt/dev/rtcSdk/files/resources/filevideo.mp4' });
+//     // await rtc.pushStream({ audio: '/opt/dev/rtcSdk/files/resources/彩虹.mp3' });
 
-    // // 推送音频（base64)
-    // const audio = fs.readFileSync('/opt/dev/rtcSdk/server/service/rtc/tts.text', 'utf-8')
-    // await rtc.pushTTS(audio);
+//     // // 推送音频（base64)
+//     const fs = require('fs');
+//     const audio = fs.readFileSync('/opt/dev/rtcSdk/server/service/rtc/tts.text', 'utf-8')
+//     console.log(rtc.peerId)
+//     await rtc.pushTTS(audio);
 
-    // // 离开房间
-    // setTimeout(() => {
-    //     rtc.leaveRoom();
-    // }, 60000);
-
-})
+//     // // 离开房间
+//     // setTimeout(() => {
+//     //     rtc.leaveRoom();
+//     // }, 60000);
+// })
 
 
 
