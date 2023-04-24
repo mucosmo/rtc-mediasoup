c27eb753cc2c84336c01efcb4a4398f40b940ef8
huanyu.yang <mfuture@qq.com>
2023-04-24 13:10:27 +0800

    update ffmpeg script

--- lab/scripts/ffmpeg.sh
+++ lab/scripts/ffmpeg.sh
@@ -171,13 +171,30 @@ ffmpeg  -i /opt/application/tx-rtcStream/files/resources/office10s.mp4  -i /opt/
 
 
 # 圆形蒙版（尝试去除背景）
-ffmpeg  -i /opt/application/tx-rtcStream/files/resources/office10s.mp4  -i /opt/application/tx-rtcStream/files/resources/dh.mp4 -filter_complex "[0:v]null[region_0.0];[1:v]null[region_1.0.1];[region_0.0]scale=w=454:h=240[region_0.0_prepro];[region_1.0.1]crop=w=300:h=300:x=130:y=130,scale=w=200:h=200[region_1.0.1_prepro];movie='/opt/application/tx-rtcStream/files/resources/mask.png'[region_1.0.1_mask];[region_1.0.1_mask]alphaextract,scale=w=200:h=200[region_1.0.1_premask];[region_1.0.1_prepro][region_1.0.1_premask]chromakey=color=0x00ff00:similarity=0.3:blend=0.05,alphamerger[region_1.0.1_maskmerge];[region_0.0_prepro][region_1.0.1_maskmerge]overlay=(W-w)/5:(H-h)/10" -c:v libx264 -y mask.mp4
+ffmpeg  -i /opt/application/tx-rtcStream/files/resources/office10s.mp4  -i /opt/application/tx-rtcStream/files/resources/dh.mp4  -i /opt/application/tx-rtcStream/files/resources/globalmap.png -filter_complex "[0:v]null[region_0.0];[1:v]null[region_1.0.1];[2:v]scale=-1:200[2v];[region_0.0]scale=w=454:h=240[region_0.0_prepro];[region_1.0.1]crop=w=300:h=300:x=130:y=130,scale=w=200:h=200[region_1.0.1_prepro];movie='/opt/application/tx-rtcStream/files/resources/mask.png'[region_1.0.1_mask];[region_1.0.1_mask]alphaextract,scale=w=200:h=200[region_1.0.1_premask];[region_1.0.1_prepro][region_1.0.1_premask]alphamerge[region_1.0.1_maskmerge];[2v][region_1.0.1_maskmerge]overlay=10:10[ov];[ov]chromakey=color=0x00ff00:similarity=0.3:blend=0[masked];[region_0.0_prepro][masked]overlay=(W-w)/5:(H-h)/10" -c:v libx264 -y mask_removed.mp4
 
-ffmpeg  -i /opt/application/tx-rtcStream/files/resources/office10s.mp4  -i /opt/application/tx-rtcStream/files/resources/dh.mp4 -filter_complex "[0:v]null[region_0.0];[1:v]null[region_1.0.1];[region_0.0]scale=w=454:h=240[region_0.0_prepro];[region_1.0.1]crop=w=300:h=300:x=130:y=130,scale=w=200:h=200[region_1.0.1_prepro];movie='/opt/application/tx-rtcStream/files/resources/mask.png'[region_1.0.1_mask];[region_1.0.1_mask]alphaextract,scale=w=200:h=200[region_1.0.1_premask];[region_1.0.1_prepro][region_1.0.1_premask]alphamerge[region_1.0.1_maskmerge];[region_0.0_prepro][region_1.0.1_maskmerge]overlay=(W-w)/5:(H-h)/10" -c:v libx264 -y mask_circle.mp4
+ffmpeg  -i /opt/application/tx-rtcStream/files/resources/office10s.mp4  -i /opt/application/tx-rtcStream/files/resources/dh.mp4 -filter_complex "[0:v]null[region_0.0];[1:v]null[region_1.0.1];[region_0.0]scale=w=454:h=240[region_0.0_prepro];[region_1.0.1]crop=w=300:h=300:x=130:y=130,scale=w=200:h=200[region_1.0.1_prepro];movie='/opt/application/tx-rtcStream/files/resources/mask.png'[region_1.0.1_mask];[region_1.0.1_mask]alphaextract,scale=w=200:h=200[region_1.0.1_premask];[region_1.0.1_prepro][region_1.0.1_premask]alphamerge[region_1.0.1_maskmerge];[region_0.0_prepro][masked]overlay=(W-w)/5:(H-h)/10" -c:v libx264 -y mask.mp4
+
+# very close
+ffmpeg  -i /opt/application/tx-rtcStream/files/resources/office10s.mp4  -i /opt/application/tx-rtcStream/files/resources/dh.mp4  -i /opt/application/tx-rtcStream/files/resources/office10s.mp4 -filter_complex "[0:v]null[region_0.0];[1:v]null[region_1.0.1];[2:v]null[2v];[region_0.0]scale=w=454:h=240[region_0.0_prepro];[region_1.0.1]crop=w=300:h=300:x=130:y=130,scale=w=200:h=200[region_1.0.1_prepro];movie='/opt/application/tx-rtcStream/files/resources/mask.png'[region_1.0.1_mask];[region_1.0.1_mask]alphaextract,scale=w=200:h=200[region_1.0.1_premask];[region_1.0.1_prepro][region_1.0.1_premask]alphamerge[region_1.0.1_maskmerge];[2v][region_1.0.1_maskmerge]overlay=(W-w)/5:(H-h)/10[ov];[ov]chromakey=color=0x00ff00:similarity=0.3:blend=0[masked];[region_0.0_prepro][masked]overlay=0:0" -c:v libx264 -y mask_removed.mp4
+
+
+ffmpeg  -i /opt/application/tx-rtcStream/files/resources/office10s.mp4  -i /opt/application/tx-rtcStream/files/resources/dh.mp4  -i /opt/application/tx-rtcStream/files/resources/transparent.png -filter_complex "[0:v]null[region_0.0];[1:v]null[region_1.0.1];[2:v]scale=-1:240[2v];[region_0.0]scale=w=454:h=240[region_0.0_prepro];[region_1.0.1]crop=w=300:h=300:x=130:y=130,scale=w=200:h=200[region_1.0.1_prepro];movie='/opt/application/tx-rtcStream/files/resources/mask.png'[region_1.0.1_mask];[region_1.0.1_mask]alphaextract,scale=w=200:h=200[region_1.0.1_premask];[region_1.0.1_prepro][region_1.0.1_premask]alphamerge[region_1.0.1_maskmerge];[2v][region_1.0.1_maskmerge]overlay=10:10[ov];[ov]chromakey=color=0x00ff00:similarity=0.3:blend=0[masked];[region_0.0_prepro][masked]overlay=(W-w)/5:(H-h)/10" -c:v libx264 -y mask_removed.mp4
+
+
+# ok but not perfect
+ffmpeg  -i /opt/application/tx-rtcStream/files/resources/office10s.mp4  -i /opt/application/tx-rtcStream/files/resources/dh.mp4  -i /opt/application/tx-rtcStream/files/resources/office10s.mp4 -filter_complex "[0:v]null[region_0.0];[1:v]null[region_1.0.1];[2:v]null[2v];[region_0.0]null[region_0.0_prepro];[region_1.0.1]crop=w=400:h=400:x=100:y=100,scale=w=100:h=100[region_1.0.1_prepro];movie='/opt/application/tx-rtcStream/files/resources/mask.png'[region_1.0.1_mask];[region_1.0.1_mask]alphaextract,scale=w=100:h=100[region_1.0.1_premask];[region_1.0.1_prepro][region_1.0.1_premask]alphamerge[region_1.0.1_maskmerge];[2v][region_1.0.1_maskmerge]overlay=0:0[ov];[ov]chromakey=color=0x00ff00:similarity=0.3:blend=0[masked];[region_0.0_prepro][masked]overlay=0:0" -c:v libx264 -y mask_removed.mp4
+
+
+ffmpeg  -i /opt/application/tx-rtcStream/files/resources/testsrc_1280x960_30p.mp4  -i /opt/application/tx-rtcStream/files/resources/dh.mp4  -i /opt/application/tx-rtcStream/files/resources/testsrc_1280x960_30p.mp4 -filter_complex "[0:v]null[region_0.0];[1:v]null[region_1.0.1];[2:v]null[2v];[region_0.0]null[region_0.0_prepro];[region_1.0.1]crop=w=400:h=400:x=100:y=100,scale=w=300:h=300[region_1.0.1_prepro];movie='/opt/application/tx-rtcStream/files/resources/mask.png'[region_1.0.1_mask];[region_1.0.1_mask]alphaextract,scale=w=300:h=300[region_1.0.1_premask];[region_1.0.1_prepro][region_1.0.1_premask]alphamerge[region_1.0.1_maskmerge];[2v][region_1.0.1_maskmerge]overlay=W-400:H-400[ov];[ov]chromakey=color=0x00ff00:similarity=0.3:blend=0[masked];[region_0.0_prepro][masked]overlay=0:0" -c:v libx264 -y mask_removed.mp4
+
+
+# 透明层
+ffmpeg  -i /opt/application/tx-rtcStream/files/resources/testsrc_1280x960_30p.mp4  -i /opt/application/tx-rtcStream/files/resources/dh.mp4  -i /opt/dev/rtcSdk/server/transparent.mp4 -filter_complex "[0:v]null[region_0.0];[1:v]null[region_1.0.1];[2:v]scale=1280:960[2v];[region_0.0]null[region_0.0_prepro];[region_1.0.1]crop=w=400:h=400:x=100:y=100,scale=w=300:h=300[region_1.0.1_prepro];movie='/opt/application/tx-rtcStream/files/resources/mask.png'[region_1.0.1_mask];[region_1.0.1_mask]alphaextract,scale=w=300:h=300[region_1.0.1_premask];[region_1.0.1_prepro][region_1.0.1_premask]alphamerge[region_1.0.1_maskmerge];[2v][region_1.0.1_maskmerge]overlay=W-400:H-400[ov];[ov]chromakey=color=0x00ff00:similarity=0.3:blend=0[masked];[region_0.0_prepro][masked]overlay=0:0" -c:v libx264 -y mask_removed.mp4
 
 
 # extract alpha channel, white is opaque, black is transparent
-ffmpeg -i /opt/dev/rtcSdk/files/resources/mask.png -vf "alphaextract" output.mp4
+ffmpeg -i /opt/application/tx-rtcStream/files/resources/mask.png -vf "alphaextract" alphaextract.mp4
 
 ,chromakey=color=0x00ff00:similarity=0.3:blend=0.05
 
@@ -194,7 +211,7 @@ ffmpeg -i /opt/dev/rtcSdk/files/resources/20_input.mp4 -i /opt/application/tx-rt
 
 
 # 生成测试用的时钟视频(需要注意format=yuv420p)
-ffmpeg -f lavfi -i "testsrc=duration=100:size=320x240:rate=30,format=yuv420p" -c:v libx264 -preset medium -t 100 -y 100_input.mp4
+ffmpeg -f lavfi -i "testsrc=duration=40:size=1280*720:rate=30,format=yuv420p" -c:v libx264 -preset fast -y 100_input.mp4
 
 # 片段裁剪 (-ss hh:mm:ss[.xxx] 位数代表精度， 00:00:05.80 / 00:00:05.800 / 5.80 是一样的)
 ffmpeg -i /opt/application/tx-rtcStream/files/resources/100_input.mp4 -ss 70.10 -t 3.60 -y clock_clip.mp4
@@ -244,3 +261,105 @@ ffmpeg -ss 10 -t 20 -i /opt/application/tx-rtcStream/files/resources/成都.mp3 
 
 # 叠加透明背景图片
 ffmpeg  -i /opt/application/tx-rtcStream/files/resources/testsrc_1280x960_30p.mp4 -i /opt/application/tx-rtcStream/files/resources/dog_couch.png -filter_complex "[1]scale=-1:800[1s];[0][1s]overlay=0:0[ret]" -map "[ret]"  -y dog.mp4
+
+# rescale and setsar since when you concat images or videos, they should have the same dimension / pixel format / pixel aspect ratio / timebase number
+ffmpeg -i  /opt/application/tx-rtcStream/files/resources/40_input.mp4 -vf scale=1200:600,setsar=1:1 -y  scale.mp4
+
+
+ffmpeg -i /opt/application/tx-rtcStream/files/resources/image2.jpg -vf "format=yuv420p" output.jpg
+ffmpeg -i /opt/application/tx-rtcStream/files/resources/image2.jpg -pix_fmt yuv420p -preset ultrafast -y output.jpg
+
+# 黑色背景视频
+ffmpeg -f lavfi -i color=c=black@0.0:s=1280x720:r=30:d=10 -pix_fmt yuva420p transparent.mp4
+
+# 不同的时间在不同的位置
+ffmpeg -i /opt/application/tx-rtcStream/files/resources/20_input.mp4  -i /opt/application/tx-rtcStream/files/resources/dog_couch.png -filter_complex "[1:v]scale=-1:100[1v];[0:v][1v]overlay=x='if(gte(t,0)*lt(t,5),0,if(gte(t,5)*lt(t,10),(2/3)*(n-150),100))':y='if(gte(t,0)*lt(t,10),(H-h),(H-h)-(H-h)/300*(n-300))'" -pix_fmt yuv420p -c:a copy -y diff_overlay.mp4
+
+
+# zoomin
+ffmpeg -i /opt/application/tx-rtcStream/files/resources/20_input.mp4 -filter:v "zoompan=z='min(max(zoom,pzoom)+0.0015,1.5)':d=100:x='iw/2-(iw/zoom/2)':y='ih/2-(ih/zoom/2)'" -ss 00:00:10 -t 5 zoomin.mp4
+
+ffmpeg -i /opt/application/tx-rtcStream/files/resources/20_input.mp4 -filter:v "zoompan=z='if(lte(mod(time,10),3),2,1)':d=1:x=iw/2-(iw/zoom/2):y=ih/2-(ih/zoom/2):fps=30" -t 20 -y zoomin.mp4
+
+# smooth
+ffmpeg -i /opt/application/tx-rtcStream/files/resources/20_input.mp4 -filter:v "zoompan=z='min(max(zoom,pzoom)+0.015,1.5)':d=100:x=iw/2-(iw/zoom/2):y=ih/2-(ih/zoom/2):s=320*240" -t 20 -y zoomin.mp4
+
+
+ffmpeg -i /opt/application/tx-rtcStream/files/resources/20_input.mp4 -filter:v "zoompan=z='if(lte(time,3),min(max(zoom,pzoom)+0.015,3),1)':d=1:x=iw/2-(iw/zoom/2):y=ih/2-(ih/zoom/2):s=320*240" -t 20 -y zoomin.mp4
+
+# zoom 时运动 (d=1)
+ffmpeg -i /opt/application/tx-rtcStream/files/resources/20_input.mp4 -vf "zoompan=z='if(lte(mod(time,10),3),2,1)':d=1:x=iw/2-(iw/zoom/2):y=ih/2-(ih/zoom/2):fps=30" -y lock_zoom.mp4
+
+ffmpeg -i /opt/application/tx-rtcStream/files/resources/20_input.mp4 -vf "zoompan=z='min(max(zoom,pzoom)+0.015,1.5)':d=1:x=iw/2-(iw/pzoom/2):y=ih/2-(ih/pzoom/2):s=320*240,reverse" -y lock_zoom.mp4
+
+
+# geq 应用1 （翻转）
+# http://underpop.online.fr/f/ffmpeg/help/geq.htm.gz
+ffmpeg -i /opt/application/tx-rtcStream/files/resources/20_input.mp4  -i /opt/application/tx-rtcStream/files/resources/image1.jpg -filter_complex "[1:v]geq='mod(p(W-X,Y)*3,255)',crop,scale=w=-1:h=100[rounded_overlay];[0:v][rounded_overlay]overlay=x=0:y=0" -c:a copy -y geq.mp4
+
+# 斑马条纹：
+# 128 + 100*sin(2*(PI/100)*(cos(PI/3)*(X-50*T) + sin(PI/3)*Y)):128:128
+# 光球， nullsrc 是黑色背景视频
+# random(1)/hypot(X-cos(N*0.07)*W/2-W/2\,Y-sin(N*0.09)*H/2-H/2)^2*1000000*sin(N*0.02):128:128
+# 
+# (p(X,Y)+(256-p(X-4,Y-4)))/2
+ffmpeg -i /opt/application/tx-rtcStream/files/resources/20_input.mp4  -i /opt/application/tx-rtcStream/files/resources/image1.jpg -filter_complex "nullsrc=s=320x240:d=10,geq='lum=random(1)/hypot(X-cos(N*0.07)*W/2-W/2\,Y-sin(N*0.09)*H/2-H/2)^2*1000000*sin(N*0.02):128:128'[rounded_overlay];[rounded_overlay]colorkey=0x000000:0.3:0.1[av];[0:v][av]overlay=x=0:y=0:enable='between(t,1,10)':alpha=1" -c:a copy -y geq.mp4
+
+ffmpeg -i /opt/application/tx-rtcStream/files/resources/20_input.mp4  -i /opt/application/tx-rtcStream/files/resources/image1.jpg -filter_complex "[1:v]geq='lum=255*gauss((X/W-0.5)*3)*gauss((Y/H-0.5)*3)/gauss(0)/gauss(0)',format=gray[rounded_overlay];[rounded_overlay]colorkey=0x000000:0.3:0.1[av];[0:v][av]overlay=x=0:y=0:enable='between(t,1,10)':alpha=1" -c:a copy -y geq.mp4
+
+
+# 列出所有像素格式
+ffmpeg -pix_fmts
+
+# side by side （左中右）
+ffmpeg -i /opt/application/tx-rtcStream/files/resources/20_input.mp4 -i /opt/application/tx-rtcStream/files/resources/forest.mp4 -i /opt/application/tx-rtcStream/files/resources/20_input.mp4 -filter_complex "[0:v]crop=w=iw/2:h=ih:x=iw/2:y=0[0v];[2:v]crop=w=iw/2:h=ih:x=iw/2:y=0,geq='255-p(X,Y)'[2v];[1:v]scale=w=-1:h=240[1v];[0v]pad=iw*3:ih[int];[int][1v]overlay=W/3:0[vid];[vid][2v]overlay=2*W/3:0[vid2]" -map "[vid2]" -c:v libx264 -crf 23 -preset veryfast -y leftcenterright.mp4
+
+# side by side （左上下）
+ffmpeg -i /opt/application/tx-rtcStream/files/resources/20_input.mp4 -i /opt/application/tx-rtcStream/files/resources/forest.mp4 -i /opt/application/tx-rtcStream/files/resources/20_input.mp4 -filter_complex "[0:v]crop=w=iw/2:h=ih:x=iw/2:y=0[0v];[2:v]crop=w=iw/2:h=ih/2:x=iw/2:y=ih/2,geq='(255-p(X,Y))*random(1)*255'[2v];[1:v]scale=w=-1:h=120[1v];[0v]pad=iw*2:ih[int];[int][1v]overlay=W/2:0[vid];[vid][2v]overlay=W/2:H/2[vid2]" -map "[vid2]" -c:v libx264 -crf 23 -preset veryfast -y leftupdown.mp4
+
+# zoom-out， 先 scale 放大，再缩小
+ffmpeg -loop 1 -i /opt/application/tx-rtcStream/files/resources/image1.jpg -vf "scale=-4:4*ih,zoompan=z='if(lte(zoom,1.0),3,max(1.001,zoom-0.02))':d=150" -c:v libx264 -t 5  -y zoom_out.mp4
+
+# 背景视频 concat (用 trim)
+ffmpeg -t 15 -i /opt/application/tx-rtcStream/files/resources/20_input.mp4 -i /opt/application/tx-rtcStream/files/resources/40_input.mp4  -t 10 -i /opt/application/tx-rtcStream/files/resources/image1.jpg -filter_complex "[0:v]trim=0:5,setpts=PTS-STARTPTS,fade=t=out:st=4:d=1[bg1];[1:v]trim=5:15,setpts=PTS-STARTPTS,fade=t=in:st=5:d=1[bg2];[2:v]scale=-1:10[2v];[bg1][bg2]concat=n=2:v=1:a=0[bg];[bg][2v]overlay=10:10" -c:a copy -y mov_with_overlay.mp4
+
+# 通过裁剪防抖
+ffmpeg -i /opt/application/tx-rtcStream/files/resources/dog_couch_orig.jpg -filter_complex "zoompan=z=zoom+0.001:fps=30:s=1920x1080, crop=1920:1080:10:10" zoom_offset.mp4
+
+# 改变播放速度 (0.5 代表 2 倍速， 2 代表 0.5 倍速, 时间变量是大写的 T)
+# https://ffmpeg.org/ffmpeg-filters.html#setpts_002c-asetpts
+# 如下并不能正确的加速部分视频，加速都是针对完整视频的，比如在 final cut pro 中，如果要加速视频的某个片段，首先必须从视频中剪切出来，然后再加速，最后再拼接回去
+ffmpeg -i /opt/application/tx-rtcStream/files/resources/20_input.mp4 -filter:v "setpts='if(gte(T,5),0.5*PTS,PTS)'" -c:a copy -y speed.mp4
+
+# 帧率
+ffmpeg -i /opt/application/tx-rtcStream/files/resources/20_input.mp4   -filter:v "fps=2" -c:a copy  -y frame_rate.mp4
+
+# 转场（dissolve, wipe, blur, circle, diagbl, zoomin效果）
+# 要求 format / scale / fps / settb 一致
+ffmpeg -i /opt/application/tx-rtcStream/files/resources/20_input.mp4 -i /opt/application/tx-rtcStream/files/resources/office10s.mp4 -filter_complex "[1:v]scale=320:240,setsar=1:1,fps=30,settb=1/15360[1v];[0:v][1v]xfade=transition=dissolve:duration=2:offset=3" -y ./files/synthesis/xfade_dissolve.mp4
+
+# 定制化（custom）转场
+# https://ffmpeg.org/ffmpeg-filters.html#xfade
+ffmpeg -i /opt/application/tx-rtcStream/files/resources/20_input.mp4 -i /opt/application/tx-rtcStream/files/resources/forest.mp4 -filter_complex "[1:v]scale=320:240,setsar=1:1,fps=30,settb=1/15360[1v];[0:v][1v]xfade=transition=custom:expr='if(gt(P, abs(X/W - 0.5)), A, B)':duration=2:offset=3" -y ./files/synthesis/xfade_dissolve.mp4
+
+# 叠加时延迟出现
+ffmpeg -i /opt/application/tx-rtcStream/files/resources/40_input.mp4 -i /opt/application/tx-rtcStream/files/resources/40_input.mp4 -filter_complex "[1:v]setpts=PTS-STARTPTS+4/TB,scale=w='iw/3':h='ih/3'[v1];[0:v][v1]overlay=10:10" -c:a copy overlay_setpts.mp4
+
+# 黑幕衔接制作背景
+ffmpeg  -i /opt/application/tx-rtcStream/files/resources/20_input.mp4 -i  /opt/application/tx-rtcStream/files/resources/forest.mp4 -filter_complex "[0:v]trim=end=5,setpts=PTS-STARTPTS,fade=t=out:st=4:d=1[v1];[1:v]trim=start=0,setpts=PTS-STARTPTS+5/TB,scale=320:240,setsar=1:1,fps=30,settb=1/15360[v3]; [v1][v3]concat=n=2:v=1:a=0" -c:v libx264  -y concat_blank.mp4
+
+# 黑色背景视频作为第0层
+ffmpeg -f lavfi -i color=c=black:s=320x240:r=30:d=5 -i /opt/application/tx-rtcStream/files/resources/20_input.mp4  -i /opt/application/tx-rtcStream/files/resources/20_input.mp4 -filter_complex "[1:v]null[1v];[0:v][1v]overlay=0:0" -y black_bg.mp4
+
+# 分屏 (黑底为背景，较快)
+ffmpeg -f lavfi -i color=c=black:s=320x240:r=30:d=5 -i /opt/application/tx-rtcStream/files/resources/20_input.mp4  -ss 0 -t 20 -i /opt/application/tx-rtcStream/files/resources/forest.mp4 -i /opt/application/tx-rtcStream/files/resources/20_input.mp4 -filter_complex "[1:v]crop=iw/3:ih:2*iw/3:0[1v];[2:v]crop=iw/3:ih:2*iw/3:0[2v];[3:v]crop=2*iw/4:ih/2:iw/2:ih/3[3v];[0:v][1v]overlay=0:0[ov1];[ov1][2v]overlay=W/3[ov2];[ov2][3v]overlay=W/3:H/2" -y -r 12 black_bg.mp4
+
+# 用 xstack 生成 mosaic 效果 （源视频大小不一时非常非常慢）
+ffmpeg -i /opt/application/tx-rtcStream/files/resources/20_input.mp4 -i /opt/application/tx-rtcStream/files/resources/20_input.mp4 -i /opt/application/tx-rtcStream/files/resources/video2.mp4 -filter_complex "[0:v]null[bg0]; [1:v]crop=iw:ih/2[bg1]; [2:v]scale=iw:ih/2[bg2];[bg0][bg1][bg2]xstack=inputs=3:layout=0_0|w0_0|w0_h1" -y xstack.mp4
+
+
+# 黑底背景分屏加转场
+ffmpeg  -i /opt/application/tx-rtcStream/files/resources/20_input.mp4  -ss 0 -t 20 -i /opt/application/tx-rtcStream/files/resources/forest.mp4 -i /opt/application/tx-rtcStream/files/resources/20_input.mp4 -i /opt/application/tx-rtcStream/files/resources/office10s.mp4 -i /opt/application/tx-rtcStream/files/resources/video2.mp4 -filter_complex "color=c=black:s=320x240:r=30:d=5[bg];[0:v]crop=iw/3:ih:2*iw/3:0[0v];[1:v]crop=iw/3:ih:2*iw/3:0[1v];[2:v]crop=2*iw/4:ih/2:iw/2:ih/3[2v];[bg][0v]overlay=0:0[ov0];[ov0][1v]overlay=W/3[ov1];[ov1][2v]overlay=W/3:H/2,settb=1/20[bg0];[3:v]scale=320:240,setsar=1:1,fps=30,settb=1/20[3v];[4:v]scale=320:240,setsar=1:1,fps=30,settb=1/20[4v];[bg0][3v]xfade=transition=circleclose:duration=2:offset=13[bg1];[bg1][4v]xfade=transition=custom:expr='if(gt(P, abs(X/W - 0.5)), A, B)':duration=1:offset=23" -y -r 12 black_bg.mp4
+
+
+ffmpeg -i /opt/application/tx-rtcStream/files/resources/20_input.mp4  -filter_complex "[0:v]null[0v];[0v][0v]concat=n=2:v=1:a=0" -y concat_same.mp4
